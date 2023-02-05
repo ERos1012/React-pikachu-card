9138057b30fbbd3401ed5523ab14c7a8c3072f79
ERos1012 <65523315+ERos1012@users.noreply.github.com>
2023-02-05 13:17:15 -0500

    Changed JS and CSS to Pikachu Card

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="main">
+      <head>
+      <link rel="stylesheet" href="./App.css"></link>
+      </head>
+      <div class='buttons'>
+      <button class="duplicateBtn">Duplicate Card</button>
+      <button class="deleteCardBtn">Delete Last Card</button>
+      <button class="changeColorBtn">Change Color</button>
+      <button class="changeTextBtn">Change Text</button>
+      </div>
+    <div class="card">
+      <button class="detailsBtn" onclick="hideText()">Details</button>
+      <h4 class="title">Pikachu</h4>
+      <img class="pikachuImage"
+         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxYVal1cEpqlLP77-gaeY8Jd_AZpKhvo9RJA&usqp=CAU">
+      </img>
+      <p class="paragraph" hidden>Pikachu, the Mouse Pokémon. It can generate electric attacks from the electric pouches located in both of its cheeks.</p>
+      </div>
     </div>
   );
 }
